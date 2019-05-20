c6f66c6090219aa75934fe8150fb3bec2c6b0fdb
Huy <huydocanthi@gmail.com>
2019-05-20 13:21:50 +0700

    Latest update

---
+++ QLNS/BLL/HopDongDAO.cs
@@ -0,0 +1,39 @@
+﻿using QLNS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNS.BLL
+{
+    class HopDongDAO
+    {
+        private static HopDongDAO instance;
+        SqlConnection conn = new SqlConnection();
+        public HopDongDAO()
+        {
+
+            string connString = @"Data Source=DESKTOP-34CKI58\HOAI;Initial Catalog=QLNS;Integrated Security=True";
+
+            conn.ConnectionString = connString;
+            conn.Open();
+        }
+        internal static HopDongDAO Instance
+        {
+            get { if (instance == null) instance = new HopDongDAO(); return instance; }
+            private set { instance = value; }
+        }
+    //    public bool Insert(HopDong cc)
+    //    {
+    //        int result;
+
+    //        HopDong nv = new HopDong();
+    //            result = DataProvider.Instance.ExecuteNonQuery("EXEC Them ", new object[] { cc.TenCty, cc.DiaChi, cc.ChuTich, cc.HotLine });
+           
+    //        return result > 0;
+    //    }
+    }
+}
---
+++ QLNS/BLL/NhatKyDAO.cs
@@ -0,0 +1,71 @@
+﻿using QLNS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNS.BLL
+{
+    class NhatKyDAO
+    {
+        private static NhatKyDAO instance;
+        SqlConnection conn = new SqlConnection();
+        public NhatKyDAO()
+        {
+
+            string connString = @"Data Source=DESKTOP-34CKI58\HOAI;Initial Catalog=QLNS;Integrated Security=True";
+
+            conn.ConnectionString = connString;
+            conn.Open();
+        }
+        public List<NhatKy> LayDL()
+        {
+            List<NhatKy> list = new List<NhatKy>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("select * from NhatKy");
+            foreach (DataRow item in data.Rows)
+            {
+                NhatKy nv = new NhatKy(item);
+                list.Add(nv);
+            }
+            return list;
+        }
+        internal static NhatKyDAO Instance
+        {
+            get { if (instance == null) instance = new NhatKyDAO(); return instance; }
+            private set { instance = value; }
+        }
+        public bool Insert(long cc)
+        {
+            int result;
+            NhatKy nv = new NhatKy();
+            result = DataProvider.Instance.ExecuteNonQuery("exec ThemNK @Ma ", new object[] { cc });
+            return result > 0;
+        }
+        public List<NhatKy> TimKiemMa(long a)
+        {
+            NhatKy nv = new NhatKy();
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC TimNKMa @ma",new object[] { a});
+            List<NhatKy> list = new List<NhatKy>();
+            foreach(DataRow d in data.Rows){
+                NhatKy nk = new NhatKy(d);
+                list.Add(nk);
+            }
+            return list;
+        }
+        public List<NhatKy> TimKiemNgay(DateTime a)
+        {
+            NhatKy nv = new NhatKy();
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC TimNKNgay @ngay", new object[] { a });
+            List<NhatKy> list = new List<NhatKy>();
+            foreach (DataRow d in data.Rows)
+            {
+                NhatKy nk = new NhatKy(d);
+                list.Add(nk);
+            }
+            return list;
+        }
+    }
+}
